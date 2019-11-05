de07730ece260739618ce6018f48fbdf386b168f
David Herrero <daheji@gmail.com>
2019-11-05 13:06:48 +0100

    Initial Commit

---
+++ GuessTheFlagFinalProject/ContentView.swift
@@ -0,0 +1,17 @@
+// Package: GuessTheFlagFinalProject,
+// File name: ContentView.swift,
+// Created by David Herrero on 05/11/2019.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
